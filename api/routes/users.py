c458970e952e66794b0ad36c6f5a5019767ee643
phTononFerreira <pehetofe@gmail.com>
2023-03-23 18:22:58 -0300

    🚩 POST de user (Teste)

--- api/routes/users.py
+++ api/routes/users.py
@@ -28,3 +28,25 @@ def get_users():
         return Response('Error Message', 400)
     
     return jsonify({"Message":"Teste"})
+
+@users_bp.route('/', methods=['POST'])
+def post_users():
+    """
+    Endpoint para listar todos os usuários
+    ---
+    responses:
+      200:
+        description: Lista de todos os usuários
+        schema:
+          type: array
+          items:
+            $ref: '#/definitions/User'
+    """
+    try:
+        data = request.get_json()
+        user = UserSchema().load(data)
+        return jsonify(user)
+    except Exception as error:
+        return Response("Erro na requisição", 400)
+    
+    
