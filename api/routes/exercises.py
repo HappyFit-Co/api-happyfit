d784de6f7e46e3919ac0c864f32fa1a32a9c817d
ViniciusGR797 <loritoamarelo@outlook.com>
2023-05-14 14:28:41 -0300

    ⛰ Swagger improvements and added routes

--- api/routes/exercises.py
+++ api/routes/exercises.py
@@ -1,5 +1,5 @@
 from flask import abort
-from flask_restx import Resource
+from flask_restx import Resource, fields
 from api.controllers.exerciseController import ExerciseController
 from api.schemas.exerciseSchema import ns, exercise_schema
 
@@ -7,19 +7,16 @@ exercise_controller = ExerciseController()
 
 @ns.route('/')
 class ExerciseList(Resource):
+    @ns.doc(description='Retorna a lista de dados de exercício')
     @ns.marshal_list_with(exercise_schema)
     @ns.expect({})
     def get(self):
-        """
-        Retorna a lista de dados de exercício
-        """
-        exercises = exercise_controller.get_all_exercises()
-        if not exercises:
-            return {'message': 'No exercises found'}, 404
-        return exercises
+        """Lista todos os exercícios"""
+        return exercise_controller.get_all_exercises()
 
 @ns.route('/id/<string:exercise_id>')
 class ExerciseById(Resource):
+    @ns.doc(params={'exercise_id': 'ID do exercício que deseja buscar'})
     @ns.marshal_with(exercise_schema)
     @ns.expect({'exercise_id': str})
     def get(self, exercise_id):
@@ -27,22 +24,21 @@ class ExerciseById(Resource):
         Retorna dados de um exercício pelo _id
         """
         exercises = exercise_controller.get_exercise_by_id(exercise_id)
-        if not exercises:
+        if exercises is None or not exercises:
             return {'message': 'No exercise found'}, 404
         return exercises
 
 @ns.route('/name/<string:exercise_name>')
 class ExerciseByName(Resource):
     @ns.marshal_with(exercise_schema)
+    #@ns.response(404, {'message': 'No exercises found'})
+    @ns.param('exercise_name', 'Nome do exercício', _in='query', required=True, type=str)
     @ns.expect({'exercise_name': str})
     def get(self, exercise_name):
         """
         Retorna dados de um exercício pelo nome
         """
-        exercises = exercise_controller.get_exercise_by_name(exercise_name)
-        if not exercises:
-            return {'message': 'No exercises found'}, 404
-        return exercises
+        return exercise_controller.get_exercise_by_name(exercise_name)
 
 @ns.route('/muscle/<string:exercise_target>')
 class ExerciseByTarget(Resource):
@@ -53,6 +49,6 @@ class ExerciseByTarget(Resource):
         Retorna lista de exercícios pelo músculo target
         """
         exercises = exercise_controller.get_exercise_by_target(exercise_target)
-        if not exercises:
+        if exercises is None or not exercises:
             return {'message': 'No exercises found'}, 404
         return exercises
