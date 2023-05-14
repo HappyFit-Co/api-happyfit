43a8f089d629bc2fe0bf2d210ebf2199a4b232f4
ViniciusGR797 <loritoamarelo@outlook.com>
2023-05-14 03:58:05 -0300

    🚧 Building exercise by ID, name and target

--- api/controllers/exerciseController.py
+++ api/controllers/exerciseController.py
@@ -8,6 +8,14 @@ class ExerciseController:
         exercises = self.exercise_service.get_all_exercises()
         return exercises
 
-    def add_new_exercise(self, new_exercise):
-        exercise = self.exercise_service.add_new_exercise(new_exercise)
+    def get_exercise_by_id(self, exercise_id):
+        exercise = self.exercise_service.get_exercise_by_id(exercise_id)
         return exercise
+
+    def get_exercise_by_name(self, exercise_name):
+        exercises = self.exercise_service.get_exercise_by_name(exercise_name)
+        return exercises
+
+    def get_exercise_by_target(self, exercise_target):
+        exercises = self.exercise_service.get_exercise_by_target(exercise_target)
+        return exercises
--- api/routes/exercises.py
+++ api/routes/exercises.py
@@ -1,3 +1,4 @@
+from flask import abort
 from flask_restx import Resource
 from api.controllers.exerciseController import ExerciseController
 from api.schemas.exerciseSchema import ns, exercise_schema
@@ -7,17 +8,51 @@ exercise_controller = ExerciseController()
 @ns.route('/')
 class ExerciseList(Resource):
     @ns.marshal_list_with(exercise_schema)
+    @ns.expect({})
     def get(self):
         """
         Retorna a lista de dados de exercício
         """
-        return exercise_controller.get_all_exercises()
+        exercises = exercise_controller.get_all_exercises()
+        if not exercises:
+            return {'message': 'No exercises found'}, 404
+        return exercises
 
-    @ns.expect(exercise_schema)
-    @ns.marshal_with(exercise_schema, code=201)
-    def post(self):
+@ns.route('/id/<string:exercise_id>')
+class ExerciseById(Resource):
+    @ns.marshal_with(exercise_schema)
+    @ns.expect({'exercise_id': str})
+    def get(self, exercise_id):
         """
-        Adiciona um novo dado à lista de exercício
+        Retorna dados de um exercício pelo _id
         """
-        new_exercise = ns.payload
-        return exercise_controller.add_new_exercise(new_exercise)
+        exercises = exercise_controller.get_exercise_by_id(exercise_id)
+        if not exercises:
+            return {'message': 'No exercise found'}, 404
+        return exercises
+
+@ns.route('/name/<string:exercise_name>')
+class ExerciseByName(Resource):
+    @ns.marshal_with(exercise_schema)
+    @ns.expect({'exercise_name': str})
+    def get(self, exercise_name):
+        """
+        Retorna dados de um exercício pelo nome
+        """
+        exercises = exercise_controller.get_exercise_by_name(exercise_name)
+        if not exercises:
+            return {'message': 'No exercises found'}, 404
+        return exercises
+
+@ns.route('/muscle/<string:exercise_target>')
+class ExerciseByTarget(Resource):
+    @ns.marshal_list_with(exercise_schema)
+    @ns.expect({'exercise_target': str})
+    def get(self, exercise_target):
+        """
+        Retorna lista de exercícios pelo músculo target
+        """
+        exercises = exercise_controller.get_exercise_by_target(exercise_target)
+        if not exercises:
+            return {'message': 'No exercises found'}, 404
+        return exercises
