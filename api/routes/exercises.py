fee781cbe03f1d6023b378b234111f3230788d3a
ViniciusGR797 <loritoamarelo@outlook.com>
2023-05-14 16:23:33 -0300

    ⛓ fixes in routes exercises

--- api/routes/exercises.py
+++ api/routes/exercises.py
@@ -16,39 +16,27 @@ class ExerciseList(Resource):
 
 @ns.route('/id/<string:exercise_id>')
 class ExerciseById(Resource):
-    @ns.doc(params={'exercise_id': 'ID do exercício que deseja buscar'})
+    @ns.doc(description='Retorna dados de um exercício pelo _id', params={'exercise_id': 'ID do exercício que deseja buscar'})
     @ns.marshal_with(exercise_schema)
     @ns.expect({'exercise_id': str})
     def get(self, exercise_id):
-        """
-        Retorna dados de um exercício pelo _id
-        """
-        exercises = exercise_controller.get_exercise_by_id(exercise_id)
-        if exercises is None or not exercises:
-            return {'message': 'No exercise found'}, 404
-        return exercises
+        """Lista exercício pelo _id"""
+        return exercise_controller.get_exercise_by_id(exercise_id)
 
 @ns.route('/name/<string:exercise_name>')
 class ExerciseByName(Resource):
+    @ns.doc(description='Retorna dados de um exercício pelo nome', params={'exercise_name': 'Nome do exercício que deseja buscar'})
     @ns.marshal_with(exercise_schema)
-    #@ns.response(404, {'message': 'No exercises found'})
-    @ns.param('exercise_name', 'Nome do exercício', _in='query', required=True, type=str)
     @ns.expect({'exercise_name': str})
     def get(self, exercise_name):
-        """
-        Retorna dados de um exercício pelo nome
-        """
+        """Lista exercício pelo name"""
         return exercise_controller.get_exercise_by_name(exercise_name)
 
 @ns.route('/muscle/<string:exercise_target>')
 class ExerciseByTarget(Resource):
+    @ns.doc(description='Retorna lista de exercícios pelo músculo target', params={'exercise_target': 'Nome do músculo target que deseja buscar'})
     @ns.marshal_list_with(exercise_schema)
     @ns.expect({'exercise_target': str})
     def get(self, exercise_target):
-        """
-        Retorna lista de exercícios pelo músculo target
-        """
-        exercises = exercise_controller.get_exercise_by_target(exercise_target)
-        if exercises is None or not exercises:
-            return {'message': 'No exercises found'}, 404
-        return exercises
+        """Lista exercício pelo músculo target"""
+        return exercise_controller.get_exercise_by_target(exercise_target)
