d7757f89a52b2d1eeb94b3d914710c0129a9351c
ViniciusGR797 <loritoamarelo@outlook.com>
2023-05-17 01:15:38 -0300

    👤 Melhoria no create user

--- api/routes/users.py
+++ api/routes/users.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restx import Resource
 
 from api.controllers.users import UserController
-from api.schemas.users import ns, user_schema
+from api.schemas.users import ns, user_schema, create_user_schema, login_user_schema
 
 user_controller = UserController()
 
@@ -17,8 +17,8 @@ class UserMe(Resource):
         return user_controller.get_user_me(get_jwt_identity())
 
     @ns.doc(description='Registra um novo usuário')
-    @ns.expect(user_schema)
     @ns.marshal_with(user_schema)
+    @ns.expect(create_user_schema)
     def post(self):
         """Cadastra um novo usuário"""
         return user_controller.create_user(request.json)
@@ -27,7 +27,7 @@ class UserMe(Resource):
 @ns.route('/login')
 class LoginUser(Resource):
     @ns.doc(description='Login do usuário')
-    @ns.expect(user_schema)
+    @ns.expect(login_user_schema)
     def post(self):
         """Faz login do usuário"""
         return user_controller.login(request)
