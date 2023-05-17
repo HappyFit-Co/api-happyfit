0e14b02f372ad9b1febda1bb0340afa910e8770f
phTononFerreira <pehetofe@gmail.com>
2023-05-17 10:17:59 -0300

    ⚠ Goals pt.2

--- api/controllers/goals.py
+++ api/controllers/goals.py
@@ -1,5 +1,27 @@
+from flask_restx import marshal
+
+from api.schemas.goals import goal_schema
 from api.services.goals import GoalService
 
-class GoalController: 
-    def __init__(self):
-        self.goal_service = GoalService()
+
+class GoalController:
+    def get_goal(user_id):
+        goal = GoalService.get_goal(user_id)
+        return marshal(goal, goal_schema)
+
+    def post_goal(user_id, data):
+        return GoalService.edit_goal(user_id, data)
+
+    def put_goal(user_id, data):
+        return GoalService.edit_goal(user_id, data)
+
+    def delete_goal(user_id):
+        return GoalService.delete_goal(user_id)
+    
+
+    def add_exercise(user_id, exercise_register):
+        return GoalService.add_exercise(user_id, exercise_register)
+    
+    def rmv_exercise(user_id, exercise_id):
+        return GoalService.rmv_exercise(user_id, exercise_id)
+
--- api/services/goals.py
+++ api/services/goals.py
@@ -1,5 +1,50 @@
-from api.utils.database import mongo
 from bson.objectid import ObjectId
 
+from api.utils.database import mongo
+
+
 class GoalService:
-    pass 
+    def get_goal(user_id):
+        user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+        return user["goal"]
+
+
+    def edit_goal(user_id, data):
+        try:
+            mongo.db.users.update_one(
+                {"_id": ObjectId(user_id)},
+                {"$set": {"goal": data}}
+            )
+            return data, 200
+        except:
+            return {'message': 'Erro ao atualizar configuração'}, 400
+
+    def delete_goal(user_id):
+        try:
+            mongo.db.users.update_one(
+                {"_id": ObjectId(user_id)},
+                {"$unset": {"goal": ""}}
+            )
+            return {'message': 'Meta excluída com sucesso'}, 200
+        except:
+            return {'message': 'Erro ao excluir a meta'}, 400
+
+    def add_exercise(user_id, data):
+        try:
+            mongo.db.users.update_one(
+                {"_id": ObjectId(user_id)},
+                {"$push": {"goal.workout": data}}
+            )
+            return {'message': 'Exercício adicionado com sucesso'}, 200
+        except:
+            return {'message': 'Erro ao adicionar exercício'}, 400
+
+    def rmv_exercise(user_id, exercise_id):
+        try:
+            mongo.db.users.update_one(
+                {"_id": ObjectId(user_id)},
+                {"$pull": {"goal.workout": {"exercise_id": exercise_id}}}
+            )
+            return {'message': 'Exercício removido com sucesso'}, 200
+        except:
+            return {'message': 'Erro ao remover exercício'}, 400
