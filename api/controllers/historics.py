0ad31c7065c5a8350b2a7b8855d05ef661938b1c
phTononFerreira <pehetofe@gmail.com>
2023-05-20 14:37:34 -0300

    📖 Rotas de histórico do usuário

--- api/controllers/historics.py
+++ api/controllers/historics.py
@@ -1,5 +1,9 @@
 from api.services.historics import HistoricService
 
+
 class HistoricController: 
-    def __init__(self):
-        self.historic_service = HistoricService()
+    def get_historic(user_id):
+        return HistoricService.get_historic(user_id)
+
+    def delete_historic(user_id):    
+        return HistoricService.clear_historic(user_id)
