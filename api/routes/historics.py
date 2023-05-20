83c7e1a93cecb76ea22a2ae854d029a3691fd53d
ViniciusGR797 <loritoamarelo@outlook.com>
2023-05-20 15:12:24 -0300

    🎈 Faltou um

--- api/routes/historics.py
+++ api/routes/historics.py
@@ -4,6 +4,7 @@ from flask_restx import Resource
 from api.controllers.historics import HistoricController
 from api.schemas.historics import ns
 
+
 @ns.route('/')
 class Historic(Resource):
     @jwt_required()
