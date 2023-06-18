b92683239e72e6e8d0a1c2546a962630b056751c
ViniciusGR797 <loritoamarelo@outlook.com>
2023-06-18 11:32:45 -0300

    🧹 Limpando código

--- config.py
+++ config.py
@@ -14,16 +14,9 @@ class Config:
     DEBUG = os.environ.get('DEBUG', True)
     HOST = os.environ.get('HOST', '0.0.0.0')
     PORT = os.environ.get('PORT', 5000)
-
-    # Configurações do Flask-RESTX
-    RESTX_MASK_SWAGGER = os.environ.get('RESTX_MASK_SWAGGER', False)
-    RESTX_VALIDATE = os.environ.get('RESTX_VALIDATE', True)
    
     # Configurações do JWT
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(
         hours=int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES', 6))
     )
-    JWT_COOKIE_SECURE  = os.environ.get('JWT_COOKIE_SECURE', True)
-
-    CORS_ENABLED = True
